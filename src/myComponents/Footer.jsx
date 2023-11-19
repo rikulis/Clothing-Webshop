16e2d6b4dcdbf7b779543d33655f4a2be2ef9467
rikulis <remesrikua@gmail.com>
2023-11-19 12:00:38 +0200

    Updated: Footer

--- src/myComponents/Footer.jsx
+++ src/myComponents/Footer.jsx
@@ -1,58 +1,66 @@
 import React from "react";
-import { Box, Flex, Text, Image, Link, VStack, HStack } from "@chakra-ui/react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
+import {
+  Container,
+  Stack,
+  IconButton,
+  Text,
+  Link,
+  HStack,
+  Image,
+} from "@chakra-ui/react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
-const Footer = () => {
-  return (
-    <Box className="footer" py={8} px={4} bg="gray.200">
-      <Flex
-        direction={["column", "column", "row"]}
-        align="center"
-        justify="space-between"
-      >
-        <VStack
-          spacing={2}
-          align={["center", "center", "flex-start"]}
-          mb={[4, 4, 0]}
-        >
-          <Text fontSize="2xl" fontWeight="bold">
-            WEARIT
-          </Text>
-          <Link href="#" textDecoration="underline">
-            Pre-Sale FAQS
-          </Link>
-          <Link href="#" textDecoration="underline">
-            About Us
-          </Link>
-          <Link href="#" textDecoration="underline">
-            Support
-          </Link>
-          <Link href="#" textDecoration="underline">
-            Resources
-          </Link>
-        </VStack>
-        <HStack spacing={4}>
-          <Image src={reactLogo} alt="React Logo" boxSize="30px" />
-          <Image src={viteLogo} alt="Vite Logo" boxSize="30px" />
-          {/* Add more logos as needed */}
-        </HStack>
-        <VStack spacing={1} align="center">
-          <Text fontSize="sm">Copyright: Riku Remes</Text>
-          <Text fontSize="sm">Build with:</Text>
-          <HStack spacing={2}>
-            <Link href="https://react.dev" isExternal>
-              <Image src={reactLogo} alt="React Logo" boxSize="20px" />
-            </Link>
-            <Link href="https://vite.dev" isExternal>
-              <Image src={viteLogo} alt="Vite Logo" boxSize="20px" />
-            </Link>
-            {/* Add more logos as needed */}
-          </HStack>
-        </VStack>
-      </Flex>
-    </Box>
-  );
-};
+const Footer = () => (
+  <Container
+    as="footer"
+    role="contentinfo"
+    py={{ base: "12", md: "16" }}
+    bg="gray.200"
+    maxW="full"
+    centerContent
+  >
+    <Stack spacing={{ base: "4", md: "5" }} align="center" w="full">
+      {/* Social Media Icons */}
+      <HStack spacing={4}>
+        <IconButton
+          as={Link}
+          href="#"
+          aria-label="LinkedIn"
+          icon={<FaLinkedin />}
+        />
+        <IconButton
+          as={Link}
+          href="#"
+          aria-label="GitHub"
+          icon={<FaGithub />}
+        />
+        <IconButton
+          as={Link}
+          href="#"
+          aria-label="Twitter"
+          icon={<FaTwitter />}
+        />
+      </HStack>
+
+      {/* Texts */}
+      <Stack spacing={2} align="center">
+        <Text fontSize="sm" color="gray.600">
+          Follow us on social media for updates
+        </Text>
+        <Text fontSize="sm" color="gray.600">
+          Info | Support | Marketing
+        </Text>
+      </Stack>
+
+      {/* Copyright and Logos */}
+      <Stack spacing={2} align="center">
+        <Text fontSize="sm" color="gray.600">
+          &copy; {new Date().getFullYear()} WEARIT, Inc. All rights
+          reserved.
+        </Text>
+      </Stack>
+    </Stack>
+  </Container>
+);
 
 export default Footer;
